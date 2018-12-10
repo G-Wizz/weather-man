649934eb626a33c7f72cb2b90b65dae7f6c78528
Greg Schmitt <gwizzprg100@gmail.com>
2018-12-10 08:25:52 -0500

    styled fonts more

--- src/FormatDate.js
+++ src/FormatDate.js
@@ -4,7 +4,11 @@ import moment from "moment";
 const FormatDate = props => {
   const { getDate } = props;
   console.log(getDate);
-  return <li>{moment(getDate).format("dddd, MMMM Do YYYY")}</li>;
+  return (
+    <li className="five-day-date">
+      {moment(getDate).format("dddd, MMMM Do YYYY")}
+    </li>
+  );
 };
 
 export default FormatDate;
